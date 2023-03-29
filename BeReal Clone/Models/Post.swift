07beb7c7ce8b5052541c8a45c8aab876794f8410
Melissa Saechao <melisaechao@gmail.com>
2023-03-28 21:40:22 -0700

    Initial Commit

---
+++ BeReal Clone/Models/Post.swift
@@ -0,0 +1,25 @@
+//
+//  Post.swift
+//  BeReal Clone
+//
+//  Created by Melissa Saechao on 3/27/23.
+//
+
+import Foundation
+import ParseSwift
+
+
+
+struct Post: ParseObject {
+    // These are required by ParseObject
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // Your own custom properties.
+    var caption: String?
+    var user: User?
+    var imageFile: ParseFile?
+}
